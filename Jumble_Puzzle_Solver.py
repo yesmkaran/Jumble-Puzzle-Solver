8da83bd621e71773c94d2eb203554dfbcc98b518
Karankumar Patel <kr.patel.419@gmail.com>
2023-10-25 16:38:34 -0400

    Added Comments

--- Jumble_Puzzle_Solver.py
+++ Jumble_Puzzle_Solver.py
@@ -1,27 +1,36 @@
 import time
 import functions as f
 
-
-found = False
+# list of .txt files to load into the dictionary
 file_names = [('AdjIndex.txt', 'ADJECTIVE'), ('AdvIndex.txt', 'ADVERB'),
               ('NounsIndex.txt', 'NOUN'), ('VerbsIndex.txt', 'VERB')]
+
+# read all txt files &
+# store word:pos pairs in dictionary
 words = f.read_files(file_names)
 
+# loop until the user presses "enter" key
 while True:
     user_input = input(
         "Enter the Jumble Puzzle Word: (<enter> to quit) ").lower()
 
-    if user_input == "":
+    if not user_input:
         break
 
     print(f"Start time: {time.strftime('%H:%M:%S', time.localtime())}")
+
+    # flag to check if a word is found in the dictionary
     found = False
+
+    # find all possible permutations of an inputted word
+    # check if the word exist in the dictionary
     possible_permutations = f.find_permutations(user_input)
     for perm in possible_permutations:
         if perm in words:
             found = True
             print(perm.upper(), words[perm])
 
+    # print "Not Found" when a word doesn't exist
     if not found:
         print("<Not found>")
 
--- functions.py
+++ functions.py
@@ -1,27 +1,43 @@
 import re
 
 
+# This function reads words from files and
+# stores them in a dictionary.
 def read_files(file_names):
     words = {}
 
+    # Iterate through the file_names
     for file_name, pos in file_names:
         with open(file_name, 'r') as f:
             indexes = f.readlines()
 
+        # Update the 'words' dictionary with words from the file.
+        # Words are extracted by splitting each line at the '|' character.
+        # Words that pass the 'has_no_numbers_and_special_chars' check are
+        # included in the dictionary.
         words.update({word.split('|')[0]: pos for word in indexes
                       if has_no_numbers_and_special_chars(word.split('|')[0])})
 
     return words
 
 
+# This function generates all permutations
+# of a given word.
 def find_permutations(word):
     all_permutations = ['']
 
+    # Generate permutations by inserting the character at different positions
+    # within each existing permutation.
     for char in word:
         all_permutations = [perm[:i] + char + perm[i:] for perm in
                             all_permutations for i in range(len(perm) + 1)]
+
     return set(all_permutations)
 
 
+# This function checks if a word contains numbers or special characters.
+# Use regular expression to search for digits, underscores, and
+# hyphens in the 'word'.If any of these characters are found, the function
+# returns False.
 def has_no_numbers_and_special_chars(word):
     return not bool(re.search(r'[\d._-]', word))
